4dd507d2f89da6fde2de764e9324111bb7884a57
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-27 14:25:25 +0100

    Add test converting py::int_ to py::float_

--- tests/test_pytypes.cpp
+++ tests/test_pytypes.cpp
@@ -209,6 +209,7 @@ TEST_SUBMODULE(pytypes, m) {
     m.def("get_tuple_from_iterable", [](const py::iterable &iter) { return py::tuple(iter); });
     // test_float
     m.def("get_float", [] { return py::float_(0.0f); });
+    m.def("test_float_from_int", [](py::int_ i){ return py::float(i); });
     m.def("float_roundtrip", [](py::float_ f) { return f; });
     // test_list
     m.def("list_no_args", []() { return py::list{}; });
--- tests/test_pytypes.py
+++ tests/test_pytypes.py
@@ -62,6 +62,9 @@ def test_iterable(doc):
 def test_float(doc):
     assert doc(m.get_float) == "get_float() -> float"
     assert doc(m.float_roundtrip) == "float_roundtrip(arg0: float) -> float"
+    f0 = m.test_float_from_int(5)
+    assert isinstance(f0, float)
+    assert f0 == 5.0
     f1 = m.float_roundtrip(5.5)
     assert isinstance(f1, float)
     assert f1 == 5.5
