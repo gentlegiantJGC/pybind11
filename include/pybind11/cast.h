4fcdbd3afc2e461711989ff01396b2ad16436c7d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-27 11:25:04 +0100

    Add constructor

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1536,6 +1536,8 @@ class type_caster<T, enable_if_t<is_pyobject<T>::value>> : public pyobject_caste
 
 template <>
 struct type_caster<float_> {
+    type_caster() : value(reinterpret_steal<float_>(handle())) {}
+	
     bool load(handle src, bool /* convert */) {
         if (isinstance<float_>(src)) {
             value = reinterpret_borrow<float_>(src);
